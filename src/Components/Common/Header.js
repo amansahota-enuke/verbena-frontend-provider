e34eeca27871119e55b6e79d22a86efde15436b2
Gaurav Thakur <gauravthakur262@gmail.com>
2021-11-11 15:35:35 +0530

    Design changes

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -10,17 +10,20 @@ const Header = ({ handleToggle }) => {
             <div className="container mx-auto xl:px-4 lg:px-4 md-px-2 sm:px-2 px-2">
                     <div className="flex justify-between items-center xl:flex-nowrap lg:flex-nowrap md:flex-wrap sm:flex-wrap flex-wrap py-4 md:space-x-10">
                         <div className="xl:flex-grow-0 lg:flex-grow-0 md:flex-grow sm:flex-grow flex-grow">
-                            <div className="flex justify-start items-center xl:justify-start lg:justify-start md:justify-start sm:justify-between justify-between">
+                            <div className="flex items-center xl:justify-start lg:justify-start md:justify-start sm:justify-between justify-between">
                                 <NavLink to="/home">
                                     <span className="sr-only">Verbena</span>
                                     <img
-                                        className="w-40"
+                                        className="xl:w-40 lg:w-40 md:w-40 sm:w-32 w-32"
                                         src="/images/logo.png"
                                         alt=""
                                     />
                                 </NavLink>
+                                <div className="xl:hidden lg:hidden md:hidden sm:block block">
+                                    <Notifications />
+                                </div>
                                 <span
-                                    className="toggle-menu ml-8 cursor-pointer"
+                                    className="toggle-menu ml-8 md:mr-4 sm:mr-4 mr-4 cursor-pointer"
                                     onClick={() => {
                                         handleToggle();
                                     }}
@@ -35,8 +38,12 @@ const Header = ({ handleToggle }) => {
                             </div>
                         </div>
                         <div className="flex items-center">
-                            <Notifications />
-                            <Dropdown />
+                            <div className="xl:block lg:block md:block sm:hidden hidden">
+                                <Notifications />
+                            </div>
+                            <div>
+                                <Dropdown />
+                            </div>
                         </div>    
                     </div>
                 </div>
