b3b06c5a0e27785a0848e1de9de7736421c59e0f
AnudeepReddy400 <anudeepreddy4545@gmail.com>
2021-12-08 10:28:47 +0530

    private route modified

--- src/Components/Common/PrivateRoute.js
+++ src/Components/Common/PrivateRoute.js
@@ -2,36 +2,40 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { TokenService } from "../../services";
 import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import selector from "../../redux/selector";
+import { SubscriptionActions } from "../../redux/slice/subscription.slice";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+    const dispatch = useDispatch()
     const token = TokenService.getToken();
     const userSetUpFeeDetails = useSelector(selector.setUpfeeDetails)
     const userSubscription = useSelector(selector.userSubscriptionDetails)
 
-    const subscriptionValidation = props => {
-        if(!userSetUpFeeDetails){
-            return <Redirect to="/signup/privacy-policy" />
-        }else if(!userSubscription){
-            return <Redirect to="/subscription" />
-        }else if(userSubscription.status !== "active"){
-            return <Redirect to="/subscription" />
-        }else{
-            return <Component {...props} />
-        }
-    }
+    // const subscriptionValidation = async props => {
+    //     await dispatch(SubscriptionActions.checkSetUpfeeDetails())
+    //     await dispatch(SubscriptionActions.checkSubscription())
+    //     if(!userSetUpFeeDetails){
+    //         return <Redirect to="/signup/privacy-policy" />
+    //     }else if(!userSubscription){
+    //         return <Redirect to="/subscription" />
+    //     }else if(userSubscription.status !== "active"){
+    //         return <Redirect to="/subscription" />
+    //     }else{
+    //         return <Component {...props} />
+    //     }
+    // }
 
-    const loginValidation=()=>{
-        return <Redirect to="/login" />
-    }
+    // const loginValidation=()=>{
+    //     return <Redirect to="/login" />
+    // }
 
     return (
         <Route
             {...rest}
             render={(props) =>
-                token ? subscriptionValidation(props) : loginValidation()
-                // token ? <Component {...props} /> : <Redirect to="/login" />
+                // token ? subscriptionValidation(props) : loginValidation()
+                token ? <Component {...props} /> : <Redirect to="/login" />
             }
         />
     );
